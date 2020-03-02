d1127ad35283bb23941ceba3b344f75674368199
Eloy Vitorio <eloy.vitorio@accenture.com>
2020-03-02 14:18:11 -0300

    Retirando imports inuteis

--- app/src/main/java/com/example/moviesmvp/features/popularmovieslist/di/PopularMoviesComponent.kt
+++ app/src/main/java/com/example/moviesmvp/features/popularmovieslist/di/PopularMoviesComponent.kt
@@ -2,7 +2,6 @@ package com.example.moviesmvp.features.popularmovieslist.di
 
 import com.example.moviesmvp.features.di.ActivityScope
 import com.example.moviesmvp.features.di.AppComponent
-import com.example.moviesmvp.features.interactor.di.InteractorModule
 import com.example.moviesmvp.features.popularmovieslist.PopularMoviesFragment
 import dagger.Component
 
